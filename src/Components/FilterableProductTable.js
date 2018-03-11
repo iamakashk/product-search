97a0d584de82486498128e02154f6122758bd38d
AKASH KAMBLE <kambleakashg@gmail.com>
2018-03-11 14:20:28 +0530

    Complete app after all the changes.

---
+++ src/Components/FilterableProductTable.js
@@ -0,0 +1,15 @@
+ import React , {Component} from "react";
+ import SearchBar from './SearchBar';
+ import ProductTable from './ProductTable';
+ class FilterableProductTable extends React.Component {
+   render() {
+     return (
+       <div>
+         <SearchBar />
+         <ProductTable products={this.props.products} />
+       </div>
+     );
+   }
+ }
+
+ export default FilterableProductTable;
